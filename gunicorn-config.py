935fea8e032097123023451e4a08410becf6a58b
pir2 <pir2@users.noreply.github.com>
2017-07-12 09:53:52 -0400

    Update file to make it work

--- gunicorn-config.py
+++ gunicorn-config.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 #http://agiliq.com/blog/2014/06/minimal-gunicorn-configuration/
-
-log-file = '-'
 preload = True
-max-requests = 10
-max-requests-jitter = 10
-bind = 'unix:/var/www/morss/morss/sock'
-worker-class = 'gevent'
-
+max_requests = 10
+max_requests_jitter = 10
+bind = 'unix:/var/www/morss/morss.sock'
+workers = 5
+worker_class = 'gevent'
 user='www-data'
 group='www-data'
 pidfile='gunicorn_pid'
 accesslog='gunicorn_access.log'
 errorlog='gunicorn_error.log'
+
